3f2248097dc2c8c41338972be3ae3b60b77bf9af
iwashi623 <wonderland.7878@gmail.com>
2024-09-13 21:04:03 +0900

    fmt

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -9,9 +10,18 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", hello)
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK")
+	})
+	e.GET("/fmt", fmtHandler)
 	e.Logger.Fatal(e.Start(":9090"))
 }
 
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func fmtHandler(c echo.Context) error {
+	fmt.Println("fmtHandler")
+	return c.String(http.StatusOK, "fmt")
 }
